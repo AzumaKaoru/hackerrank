7ec409c4e21902aa1b57ebeb100bd488c494f31b
AzumaKaoru <ruruanprivate@gmail.com>
2019-08-28 01:35:10 +0900

    diagonal_differenceをMatrixで改善した

--- src/algorithm/diagonal_difference.rb
+++ src/algorithm/diagonal_difference.rb
@@ -1,13 +1,7 @@
+require 'matrix'
+
 class DiagonalDifference
-  def diagonal_difference(array)
-    primary_diagonal  = array.map.with_index { |ar, index|
-      ar[array.size - (index + 1)]
-    }.sum
-
-    secondary_diagonal  = array.map.with_index { |ar, index|
-      ar[index]
-    }.sum
-
-    (primary_diagonal - secondary_diagonal).abs
+  def diagonal_difference(arr)
+    (Matrix.columns(arr).trace - Matrix.columns(arr.reverse).trace).abs
   end
 end
